d2828c273f3dc07fcb9a75a58edcb9a4e297fb14
MUSTAFA ÇAY <mcay51@gmail.com>
2024-09-25 22:18:54 +0300

    notification service docker compose blog service

---
+++ notification-service/src/test/java/tr/com/mcay/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.mcay.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
